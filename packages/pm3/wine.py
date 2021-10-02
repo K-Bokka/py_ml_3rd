e397b8457c0da11a58715842b30ac34930b92bbb
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-10-02 12:17:25 +0900

    Add Bagging ensamble

--- packages/pm3/wine.py
+++ packages/pm3/wine.py
@@ -7,9 +7,14 @@ import pandas as pd
 class Wine:
 
     def __init__(self):
-        df = pd.read_csv('../../data/wine/wine.data', header=None)
-        X = df.iloc[:, 1:].values
-        y = df.iloc[:, 0].values
+        self.df = pd.read_csv('../../data/wine/wine.data', header=None)
+        self.df.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                           'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+                           'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+                           'OD280/OD315 of diluted wines', 'Proline']
+
+        X = self.df.iloc[:, 1:].values
+        y = self.df.iloc[:, 0].values
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, y, test_size=0.3, random_state=0, stratify=y)
@@ -18,6 +23,9 @@ class Wine:
         sc.fit(self.X_train)
         self.X_train_std = sc.transform(self.X_train)
         self.X_test_std = sc.transform(self.X_test)
+
+    def all_datasets(self):
+        return self.df
 
     def train_datasets(self):
         return [self.X_train, self.y_train]
